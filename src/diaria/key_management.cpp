f43ca2a1ba54c2ab4949fa832ff150dfb82ab6c2
Lukas Wölfer <lukas.woelfer@rwth-aachen.de>
2024-11-04 02:35:25 +0100

    Failing on missing keys

--- src/diaria/key_management.cpp
+++ src/diaria/key_management.cpp
@@ -21,6 +21,7 @@ auto load_symkey(const std::filesystem::path& file_path) -> symkey_t
   symkey_t symkey {};
   std::ifstream symkey_file((file_path).c_str(),
                             std::ios::in | std::ios::binary);
+  symkey_file.exceptions(std::ifstream::badbit | std::ifstream::failbit);
   symkey_file.read(make_signed_char(symkey.data()), symkey.size());
   return symkey;
 }
@@ -30,6 +31,8 @@ auto load_pubkey(const std::filesystem::path& file_path) -> public_key_t
   public_key_t pubkey {};
   std::ifstream pubkey_file((file_path).c_str(),
                             std::ios::in | std::ios::binary);
+  pubkey_file.exceptions(std::ifstream::badbit | std::ifstream::failbit);
+
   pubkey_file.read(make_signed_char(pubkey.data()), pubkey.size());
   return pubkey;
 }
@@ -40,9 +43,9 @@ auto load_private_key(const std::filesystem::path& file_path,
   stored_secret_key::serialized_key_t stored_private_key_raw {};
   std::ifstream private_key_file((file_path).c_str(),
                                  std::ios::in | std::ios::binary);
+  private_key_file.exceptions(std::ifstream::badbit | std::ifstream::failbit);
   private_key_file.read(make_signed_char(stored_private_key_raw.data()),
                         stored_private_key_raw.size());
   stored_secret_key const stored_private_key(stored_private_key_raw);
   return stored_private_key.extract_key(password);
 }
-
